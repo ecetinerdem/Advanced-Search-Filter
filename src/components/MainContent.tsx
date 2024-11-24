34e20f2f8e23aa015d8dbf6f8bc45407b554fe8a
ecetinerdem <cetoerdem1@gmail.com>
2024-11-25 01:20:08 +0400

    add pagination

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -61,7 +61,32 @@ const MainContent = () => {
 
     const filteredProducts = getFilteredProducts()
 
+    const totalProducts = 100;
+    const totalPages = Math.ceil(totalProducts/itemsPerPage);
+    const handlePageChange = (page: number) => {
+        if (page > 0 && page <= totalPages) {
+            setCurrentPage(page)
+        }
+    }
 
+    const getPaginationButtons = () => {
+        const buttons: number[] = [];
+        let startPage = Math.max(1, currentPage - 2);
+        let endPage = Math.min(totalPages, currentPage + 2);
+
+        if (currentPage - 2 < 1) {
+            endPage = Math.min(totalPages, endPage + (2 - currentPage - 1));
+        }
+        if (currentPage + 2 > totalPages) {
+            startPage = Math.min(1, startPage - (2 - totalPages - currentPage));
+        }
+
+
+        for (let page = startPage; page <= endPage; page++) {
+            buttons.push(page);
+        }
+        return buttons;
+    };
 
 
 
@@ -90,6 +115,15 @@ const MainContent = () => {
                     ))}
                 </div>
                 {/* Pagination */}
+                <div className="flex flex-col sm:flex-row justify-between items-center mt-5">
+                    <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="border px-4 py-2 mx-2 rounded-full">Previous</button>
+                    <div className="flex flex-wrap justify-center">
+                        {getPaginationButtons().map(page =>(
+                            <button key={page} onClick={() => handlePageChange(page)} className={`border px-4 py-2 mx-1 rounded-full ${page === currentPage ? "bg-black text-white": ""}`}>{page}</button>
+                            ))}
+                    </div>
+                    <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className="border px-4 py-2 mx-2 rounded-full">Next</button>
+                </div>
             </div>
         </section>
     )
